15ed6c387436b3aa2c2521051817ded8dc4c30a7
Kate Wong <katewong@Kates-MacBook-Pro.local>
2018-01-12 18:52:04 -0500

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Kate Wong on 2018-01-12.
+//  Copyright © 2018 Individual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
